258b1f2d556627f9ec15d94853ecc663785d679f
Fernando Tsuda <ftsuda-senac@users.noreply.github.com>
2018-08-09 11:45:43 -0300

    Exemplo básico
    Exemplo básico de ReactJS

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ContentView from './components/ContentView';
 
 import './styles.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return <ContentView />;
+  }
 }
 
 const rootElement = document.getElementById('root');
